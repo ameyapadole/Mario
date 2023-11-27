354c7e3751c70c9392e46b028227dd92a85d5d90
Ameya <ameyapadole26@gmail.com>
2023-11-27 13:10:59 -0800

    update

--- game.js
+++ game.js
@@ -1,54 +1,223 @@
 kaboom({
   global: true,
   fullscreen: true,
-  scale: 1,
+  scale: 2,
   debug: true,
   clearColor: [0, 0, 0, 1],
 });
 
+const MOVE_SPEED = 120
+const JUMP_FORCE = 360
+const BIG_JUMP_FORCE = 550
+const ENEMY_SPEED = 20
+let isJumping = true
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+const FALL_DEATH = 400 
 
-loadRoot('https://i.imgur.com/')
-loadSprite('coin', 'wbKxhcd.png')
-loadSprite('evil-shroom', 'KPO3fR9.png')
-loadSprite('brick', 'pogC9x5.png')
-loadSprite('block', 'M6rwarW.png')
-loadSprite('mario', 'Wb1qfhK.png')
-loadSprite('mushroom', '0wMd92p.png')
-loadSprite('surprise', 'gesQ1KP.png')
-loadSprite('unboxed', 'bdrLpi6.png')
-loadSprite('pipe-top-left', 'ReTPiWY.png')
-loadSprite('pipe-top-right', 'hj2GK4n.png')
-loadSprite('pipe-bottom-left', 'c1cYSbt.png')
-loadSprite('pipe-bottom-right', 'nqQ79eI.png')
+loadRoot("https://i.imgur.com/");
+loadSprite("coin", "wbKxhcd.png");
+loadSprite("evil-shroom", "KPO3fR9.png");
+loadSprite("brick", "pogC9x5.png");
+loadSprite("block", "M6rwarW.png");
+loadSprite("mario", "Wb1qfhK.png");
+loadSprite("mushroom", "0wMd92p.png");
+loadSprite("surprise", "gesQ1KP.png");
+loadSprite("unboxed", "bdrLpi6.png");
+loadSprite("pipe-top-left", "ReTPiWY.png");
+loadSprite("pipe-top-right", "hj2GK4n.png");
+loadSprite("pipe-bottom-left", "c1cYSbt.png");
+loadSprite("pipe-bottom-right", "nqQ79eI.png");
 
+loadSprite('blue-block', 'fVscIbn.png')
+loadSprite('blue-brick', '3e5YRQd.png')
+loadSprite('blue-steel', 'gqVoI2b.png')
+loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+loadSprite('blue-surprise', 'RMqCc1G.png')
 
+scene("game", ({level, score}) => {
+  layers(["bg", "obj", "ui"], "obj");
 
-scene("game", () => {
-  layers(['bg', 'obj', 'ui'], 'obj')
-
-  const map = [
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '                                                                         ',
-    '====================================================   ==================',
+  const maps = [[
+    "                                                                         ",
+    "                                                                         ",
+    "                                                                         ",
+    "                                                                         ",
+    "                                                                         ",
+    "                                                                         ",
+    "                                                                         ",
+    "         %     =*=%=                                                     ",
+    "                                                                         ",
+    "                                                 -+                      ",
+    "                             ^    ^     ^        ()                      ",
+    "========================   =========================   ==================",
+  ],
+  [
+    "                                    ",
+    "                                    ",
+    "                                    ",
+    "                                    ",
+    "                                    ",
+    "  %  %  === * ===    -+              ",
+    "                     ()             ",
+    "======================= =========== ",
   ]
+];
 
   const levelConfig = {
-    width: 20, 
+    width: 20,
     height: 20,
-    '=': [sprite('block', solid())],
+    "=": [sprite("block"), solid()],
+    "$": [sprite("coin"),'coin'],
+    "%": [sprite("surprise"), solid(), "coin-surprise"],
+    "*": [sprite("surprise"), solid(), "mushroom-surprise"],
+    "}": [sprite("unboxed"), solid()],
+    "(": [sprite("pipe-bottom-left"), solid(), scale(0.5)],
+    ")": [sprite("pipe-bottom-right"), solid(), scale(0.5)],
+    "-": [sprite("pipe-top-left"), solid(), scale(0.5), 'pipe'],
+    "+": [sprite("pipe-top-right"), solid(), scale(0.5), 'pipe'],
+    "^": [sprite("evil-shroom"), solid(), 'dangerous', body()],
+    "#": [sprite("mushroom"), solid(), 'mushroom', body()],  //Body() adds gravity
+    '!': [sprite('blue-block'), solid(), scale(0.5)],
+    '£': [sprite('blue-brick'), solid(), scale(0.5)],
+    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+    '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+    'x': [sprite('blue-steel'), solid(), scale(0.5)],
+
+  };
+
+  const gameLevel = addLevel(maps[level], levelConfig); //Kaboom Method
+
+  const scoreLabel = add([
+    text(score),
+    pos(30, 6),
+    layer("ui"),
+    {
+      value: score,
+    },
+  ]);
+
+  add([text("level " + parseInt(level + 1)), pos(40, 6)]);
+
+  function big() {
+    let timer = 0;
+    let isBig = false;
+    return {
+      update() {
+        if (isBig) {
+          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+          timer -= dt(); //Delta time, this is a kaboom method.
+          if (timer <= 0) {
+            this.smallify();
+          }
+        }
+      },
+      isBig() {
+        return isBig;
+      },
+      smallify() {
+        this.scale = vec2(1);
+        CURRENT_JUMP_FORCE = JUMP_FORCE
+        timer = 0;
+        isBig = false;
+      },
+
+      biggify() {
+        this.scale = vec2(2);
+        timer = time;
+        isBig = true;
+      },
+    };
   }
+  const player = add([
+    sprite("mario"),
+    solid(),
+    pos(30, 0),
+    body(),
+    big(),
+    origin("bot"),
+  ])
+ 
+  action('mushroom', (m) => {
+    m.move(35, 0)
+  })
 
-  const gameLevel = addLevel(map, levelConfig)
+  player.on("headbump", (obj) => {
+    if (obj.is('coin-surprise')){
+      gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn("}", obj.gridPos.sub(0, 0))
+    } 
 
-})
+    if (obj.is('mushroom-surprise')){
+      gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn("}", obj.gridPos.sub(0, 0))
+    } 
+  })
 
-start("game")
+  player.collides('mushroom', (m) =>{
+    destroy(m)
+    player.biggify(6)
+  })
+
+  player.collides('coin', (c) => {
+    destroy(c)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })
+
+  action('dangerous', (d) => {
+    d.move(- ENEMY_SPEED, 0)
+  })
+
+  player.collides('dangerous', (d) =>{
+
+    if(isJumping) {
+      destroy(d)
+    }
+    else{
+      go('lose', {score : scoreLabel.value})
+    }
+  })
+  keyDown("left", () => {
+    player.move(-MOVE_SPEED, 0);
+    //x axis, y axis, the move speed is negative since we are moving left on the axis
+  });
+
+  keyDown("right", () => {
+    player.move(MOVE_SPEED, 0);
+    //x axis, y axis, the move speed is negative since we are moving left on the axis
+  });
+
+  player.action(() => {
+    camPos(player.pos)
+    if(player.pos.y >= FALL_DEATH)
+    {
+      go('lose', {score: scoreLabel.value})
+    }
+  })
+  player.action(() => {
+    if(player.grounded()){
+      isJumping = false
+    }
+  })
+  player.collides('pipe', () =>{
+    keyPress('down', () => {
+      go('game', {
+        score: scoreLabel.value,
+        level: (level + 1)
+      })
+    })
+  })
+  keyPress("space", () => {
+    if (player.grounded()) {
+      isJumping = true
+      player.jump(CURRENT_JUMP_FORCE);
+    }
+  });
+});
+
+scene('lose', ({score})=> {
+  add(text(score, 32), origin('center'), pos(width()/2, height()/ 2))
+}) 
+start("game", {level: 0, score : 0});
